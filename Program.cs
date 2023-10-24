c98588e45fa2c81baee163650935cb6ccb36784c
Алексей Дударев <Dudarev.Aleksey@orenmin.onmicrosoft.com>
2023-10-24 16:57:46 +0500

    десоциализация не работает

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ using Newtonsoft.Json;
 
 ApplicationContext db = new ApplicationContext();
 
-    // Чтение JSON файла
-    var json = File.ReadAllText("./start_data.json");
-
-    // Десериализация JSON в объекты
-    var paperSizes = JsonConvert.DeserializeObject<List<Paper_sizes>>(json); // Замените на вашу модель
+// Чтение JSON файла
+var json = File.ReadAllText($"../../../start_data.json");
+Start_data models = JsonConvert.DeserializeObject<Start_data>(json);
+// Десериализация JSON в объекты
+//var paperSizes = JsonConvert.DeserializeObject<List<Paper_sizes>>(json); // Замените на вашу модель
 
     // Добавление данных в базу данных
-    db.AddRange(paperSizes); // Замените на ваш DbSet
+    db.AddRange(models.Print_colors); // Замените на ваш DbSet
     db.SaveChanges();
 
 Console.WriteLine("Start DB");
---
+++ Start data.cs
@@ -0,0 +1,15 @@
+﻿using Models_For_EF_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models_For_EF_Core
+{
+    public class Start_data
+    {
+        public List<Print_colors> Print_colors;
+        public List<Paper_sizes> Paper_sizes;
+    }
+}
