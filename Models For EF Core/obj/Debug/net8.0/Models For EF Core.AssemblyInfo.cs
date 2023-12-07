af95685adc97aa72f67b2240ab836d7d6c01721c
Алексей Дударев <Dudarev.Aleksey@orenmin.onmicrosoft.com>
2023-12-07 16:11:02 +0500

    косметика в create, перенес то что касается авторизации авторизацию на верх страницы

--- Models For EF Core/obj/Debug/net8.0/Models For EF Core.AssemblyInfo.cs
+++ Models For EF Core/obj/Debug/net8.0/Models For EF Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models For EF Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e91c74792aa6a0a5e9bf1e3fcc21bfae3a7e0146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models For EF Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models For EF Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
